83b6ce5a1d0de63eb7b23db0cc5b374254ed7994
praveen kumar <praveen.indana1995@gmail.com>
2021-05-04 10:10:11 +0530

    changed path in controller

--- src/main/java/com/example/demo/TempController.java
+++ src/main/java/com/example/demo/TempController.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TempController 
 {
-	@RequestMapping("/test")
+	@GetMapping("/test")
 	public String sampleMethod()
 	{
 		return "hello";
