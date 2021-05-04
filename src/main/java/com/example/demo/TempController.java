dc294795ad057ff5ebf96b7a1c81db63f5b4fa30
praveen kumar <praveen.indana1995@gmail.com>
2021-05-04 13:35:33 +0530

    changed the code accordingly to sockjs

--- src/main/java/com/example/demo/TempController.java
+++ src/main/java/com/example/demo/TempController.java
@@ -1,16 +1,25 @@
 package com.example.demo;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:4200")
 public class TempController 
 {
-	@GetMapping("/test")
-	public String sampleMethod()
+	@MessageMapping("/chat.sendMessage")
+	@SendTo("/topic/public")
+	public String sendMessage(String chatmessage)
 	{
-		return "hello";
+		System.out.println(chatmessage);
+		return chatmessage;
 	}
 
 }
--- src/main/java/com/example/demo/WebSocketConfiguration.java
+++ src/main/java/com/example/demo/WebSocketConfiguration.java
@@ -1,26 +1,51 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfiguration implements WebSocketConfigurer
+@EnableWebSocketMessageBroker
+//public class WebSocketConfiguration implements WebSocketConfigurer
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer
 {
 
+	/*
+	 * @Override public void registerWebSocketHandlers(WebSocketHandlerRegistry
+	 * registry) { registry.addHandler(new WebSocketChatHandler(),
+	 * "/chat").setAllowedOrigins("*");
+	 * 
+	 * }
+	 */
+
 	@Override
-	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) 
-	{
-		registry.addHandler(new WebSocketChatHandler(), "/chat").setAllowedOrigins("*");
-		
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws").setAllowedOrigins("https://silly-galileo-ac5ec3.netlify.app").setAllowedOrigins("*").withSockJS();
 	}
 
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.setApplicationDestinationPrefixes("/app");
+		registry.enableSimpleBroker("/topic");
+	}
 	
+	 @Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**").allowedMethods("*").allowCredentials(true);
+	            }
+	        };
+	    }
     
 }
