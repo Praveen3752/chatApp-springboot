23c0724c9e0f1a8a62cf775faa196c895fcd5f6a
praveen kumar <praveen.indana1995@gmail.com>
2021-05-04 14:37:56 +0530

    handled cross origin error

--- src/main/java/com/example/demo/TempController.java
+++ src/main/java/com/example/demo/TempController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://silly-galileo-ac5ec3.netlify.app")
 public class TempController 
 {
 	@MessageMapping("/chat.sendMessage")
